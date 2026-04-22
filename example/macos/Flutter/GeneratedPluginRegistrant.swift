b07dc5c65035275aa817fcfa2f55d435ff50a786
renan jegouzo <renan@aestesis.org>
2026-04-22 17:28:40 +0200

    lil clean

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import aestesis_engine
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AestesisEnginePlugin.register(with: registry.registrar(forPlugin: "AestesisEnginePlugin"))
+}
