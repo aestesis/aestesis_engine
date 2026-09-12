63bcf65dae3dfa69534f5ae9511ab99f5853ab1a
renan jegouzo <renan@aestesis.org>
2026-09-12 03:27:27 +0200

    ok computer

--- darwin/aestesis_engine/Sources/aestesis_engine/graphics/fluid.simulation.swift
+++ darwin/aestesis_engine/Sources/aestesis_engine/graphics/fluid.simulation.swift
@@ -8,7 +8,7 @@ import simd
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 public class FluidSimulation: NodeUI {
-    var diffusion: Double = 0.1
+    var diffusion: Double = 0.01
     let size: SizeI
     let diffuseKernel: ComputeKernel
     let boundaryKernel: ComputeKernel
@@ -19,54 +19,63 @@ public class FluidSimulation: NodeUI {
     var destination: Texture2D {
         return textures[1]
     }
-
+    func swap() {
+        textures.swapAt(0, 1)
+    }
     init(parent: NodeUI, size: SizeI) throws {
         let library = parent.viewport!.library(bundle: Bundle.aestesis, name: "default")
         self.size = size
-        diffuseKernel = try ComputePass.register(
-            kernel: "kernelFluidDiffuse",
-            library: library)
-        boundaryKernel = try ComputePass.register(
-            kernel: "kernelFluidBoundary",
-            library: library)
+        diffuseKernel = try ComputePass.register(kernel: "kernelFluidDiffuse", library: library)
+        boundaryKernel = try ComputePass.register(kernel: "kernelFluidBoundary", library: library)
         super.init(parent: parent)
     }
 
     func step(dtime: Double) {
+        var fence: MTLFence?
         for _ in 1...6 {
-            diffuse(dtime: dtime)
-            // TODO: manage fence or something else
-            textures.swapAt(0, 1)
+            fence = diffuse(dtime: dtime, wait: fence)
+            fence = boundary(wait: fence)
+            swap()
         }
-
     }
 
-    func diffuse(dtime: Float, fn: ((ComputePass.Result) -> Void)? = nil) {
-        guard let viewport = viewport else { return }
-        let a = Float(dtime * diffusion) * Float(size * size)
+    func diffuse(dtime: Double, wait: MTLFence? = nil, fn: ((ComputePass.Result) -> Void)? = nil)
+        -> MTLFence?
+    {
+        guard let viewport = viewport else { return nil }
+        let a = Float(dtime * diffusion) * Float((size - SizeI(2, 2)).surface)
         let c = 1 + 4 * a
         let compute = ComputePass(parent: viewport)
         compute.use(kernel: diffuseKernel)
         compute.use(texture: destination)
-        compute.use(params: FluidDiffuseParams(a: 0, c: 0))
-        compute.use(size: size.mtl, threads: MTLSize(width: 8, height: 8, depth: 1))
+        compute.use(params: FluidDiffuseParams(a: a, c: c))
+        if let fence = wait {
+            compute.wait(fence: fence)
+        }
+        compute.dispatch(size: size.mtl, threads: MTLSize(width: 8, height: 8, depth: 1))
+        let fence = compute.update()
         compute.onDone.once { result in
             fn?(result)
         }
         compute.commit()
+        return fence
     }
 
-    func boundary(fn: ((ComputePass.Result) -> Void)? = nil) {
-        guard let viewport = viewport else { return }
+    func boundary(wait: MTLFence? = nil, fn: ((ComputePass.Result) -> Void)? = nil) -> MTLFence? {
+        guard let viewport = viewport else { return nil }
         let compute = ComputePass(parent: viewport)
-        compute.use(kernel: diffuseKernel)
+        compute.use(kernel: boundaryKernel)
         compute.use(texture: destination)
-        compute.use(params: FluidDiffuseParams(a: 0, c: 0))
-        compute.use(size: size.mtl, threads: MTLSize(width: 8, height: 8, depth: 1))
+        if let fence = wait {
+            compute.wait(fence: fence)
+        }
+        compute.dispatch(size: size.mtl, threads: MTLSize(width: 8, height: 8, depth: 1))
+        let fence = compute.update()
         compute.onDone.once { result in
             fn?(result)
         }
         compute.commit()
+        return fence
     }
 
 }
